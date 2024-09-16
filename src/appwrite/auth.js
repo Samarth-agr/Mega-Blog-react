8a8de7ca2f23c90486109e4aaae2c2175d9f84c8
samarth-agr <samarthagrawal180@gmail.com>
2024-09-16 14:42:31 +0530

    auth

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import conf from "../config/config";
+import { Client, Account} from 'appwrite';
+
+export class AuthService{
+    Client = new Client();
+    account;
+    constructor(){
+        this.Client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProject)
+        this.account = new Account(this.Client);
+    }
+
+    //Sign up
+    async CreateAccount({email,password,name}){
+        try{
+        const userAccount = await this.account.create(ID.unique(),email,password,name);
+        if(userAccount){
+            //login directly after creation
+            return this.login(email,password);
+        }
+        else{
+            return userAccount;
+        }
+    }
+        catch{
+           throw error;
+        }
+    }
+
+    //login
+
+    async login({email,password}){
+        try{
+            return await this.account.createEmailPasswordSession(email,password);
+        }
+        catch{
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }
+        catch{
+            throw error;
+        }
+    }
+    async logout(){
+        try{
+            await this.account.deleteSessions();
+        }
+        catch{
+            throw error;
+        }
+    }
+}
+
+
+const authservice = new AuthService();
+
+export default authservice;
