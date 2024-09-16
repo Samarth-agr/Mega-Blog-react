1e931024d5e6f4346f9131f374b5b2d71da1326c
samarth-agr <samarthagrawal180@gmail.com>
2024-09-16 14:44:37 +0530

    auth

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl : String("https://cloud.appwrite.io/v1"),
+    appwriteDatabase :"66e7bc7d0028b493a38c",
+    appwriteBucket : "66e7e77d0021363d43b1",
+    appwriteCollection : "66e7bcbc00267819cb94",
+    appwriteProject : "66e7bba60019ec605949"
+}
+
+export default conf;
