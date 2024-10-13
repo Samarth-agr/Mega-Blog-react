05373182ee170088198833c8ea9d34637c13eaf9
samarth-agr <samarthagrawal180@gmail.com>
2024-10-13 20:22:08 +0530

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,28 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import Store from './store/store.js'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
+
+const router = createBrowserRouter([{
+  path : '/',
+  element : <App/>,
+  children : [
+    {
+      path : '/',
+      element : <Home/>
+    },
+    {
+      path : '/login',
+      element : (
+        <AuthLayout authentication ={false}>
+          <Login/>
+        </AuthLayout>
+      )
+    }
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
